07f14f55499f033e560176bfb9dc6f427a9b06c3
kaysy2k <koushubhy2k@gmail.com>
2024-05-15 11:13:38 +0530

    start script

--- backend/index.js
+++ backend/index.js
@@ -11,4 +11,7 @@ app.use(cors());
 //You can use the body-parser npm library, or use express.json 
 app.use(express.json());
 app.use("/api/v1", rootRouter);
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server started on port 3000")
+
+});
