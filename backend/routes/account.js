1803e1f1cfb25259db6a9158f0ab4146953faf34
kaysy2k <koushubhy2k@gmail.com>
2024-05-15 11:09:05 +0530

    session transaction

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { authMiddleware } = require('../middleware');
 const { Account } = require('../db');
+const { default: mongoose } = require('mongoose');
 
 const router = express.Router();
 
@@ -15,28 +16,62 @@ router.get("/balance", authMiddleware, async (req, res) => {
 });
 
 router.post("/transfer", authMiddleware, async (req, res) => {
+
+    const session = mongoose.startSession();
+
+    (await session).startTransaction();
+
     const { amount, to } = req.body;
     
+    // Find the sender account
     const account = await Account.findOne({
         userId: req.userId
-    });
+    }).session(session);
 
+    // if balance is less than the amount
     if(account.balance < amount) {
+        (await session).abortTransaction();
         return res.status(400).json({
             message: "Insufficient Balance"
         })
     }
 
+    // Find the receiver account
     const toAccount = await Account.findOne({
         userId: to
-    });
+    }).session(session);
 
+    // if the receiver account doesnt exist
     if(!toAccount) {
+        (await session).abortTransaction();
         return res.status(400).json({
             message: "Invalid Account"
         })
     }
 
+    //Perform the transfer
+    await Account.updateOne({
+        userId: req.userId
+    }, {
+        $inc: {
+            balance: -amount
+        }
+    }).session(session);
+
+    await Account.updateOne({
+        userId: to
+    }, {
+        $inc: {
+            balance: amount
+        }
+    }).session(session);
+
+    // Commit the transaction
+    (await session).commitTransaction();
+    
+    res.json({
+        message: "Transfer Successful"
+    });
 
 });
 
